b3a08bdfa0281f008be56c84f6d65ddef8b614fb
sanlengren <ck_salary@sina.com>
2018-09-09 20:05:51 +0800

    Pilot-012 Test Git Case A

---
+++ src/test/com/kelvin/testbase/TestGitBehavior.java
@@ -0,0 +1,18 @@
+package com.kelvin.testbase;
+
+public class TestGitBehavior {
+	
+	public void testcase1(){
+		//Case - 1
+		//A 本地创建分支，提交了多次，但是没有push,想看本地记录。提交后，是否也能看到这些记录？
+		// first do add
+		// change and add again?
+		
+		
+		
+		
+	}
+	
+
+
+}
